c145f8d206bc9cf5ae03cef89936a77722471b12
Ivanov-Anton <anton.i@didww.com>
2020-12-15 14:23:19 +0200

    refsctor rails_template

--- spec/rails_template.rb
+++ spec/rails_template.rb
@@ -8,42 +8,16 @@ generate :model, 'Article title:string body:text author_id:integer published:boo
 generate :model, 'Tag name:string visible:boolean article_id:integer'
 generate :model, 'ModelWithoutTermScope name:string text:string body:string'
 
-remove_dir 'spec'
-
-# initializers ransack files
-create_file 'config/initializers/ransack.rb' do <<-RUBY
-  # frozen_string_literal: true
-  Ransack.configure do |config|
-  # Accept my custom scope values as what they are
-  config.sanitize_custom_scope_booleans = false
-end
-RUBY
-end
-
-# Add our local Active Admin to the load path
-insert_into_file 'config/environment.rb', after: "require_relative 'application'" do <<-RUBY
-
-$LOAD_PATH.unshift("#{File.expand_path(File.join(File.dirname(__FILE__),'..', 'lib'))}")
-require 'active_admin'
-RUBY
-end
-
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 generate :'active_admin:install --skip-users'
+rake 'db:migrate'
+generate :'active_admin:resource Author'
 generate :'formtastic:install'
 generate :'draper:install'
 generate :'decorator Author'
 
 remove_dir 'spec'
-
-create_file 'app/admin/authors.rb' do <<-RUBY
-  ActiveAdmin.register Author do
-    decorate_with AuthorDecorator
-    active_admin_search!
-  end
-RUBY
-end
 
 insert_into_file 'app/decorators/application_decorator.rb', after: "ApplicationDecorator < Draper::Decorator\n" do <<-RUBY
   def show_decorated_id
@@ -54,12 +28,6 @@ RUBY
 end
 
 route "root :to => 'admin/dashboard#index'"
-
-rake 'db:migrate'
-
-gsub_file "config/boot.rb", /^.*BUNDLE_GEMFILE.*$/, <<-RUBY
-  ENV['BUNDLE_GEMFILE'] = "#{File.expand_path(ENV['BUNDLE_GEMFILE'])}"
-RUBY
 
 def generate_bundler_binstub
   # disable default behaviour
